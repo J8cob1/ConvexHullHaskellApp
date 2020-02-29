668876d2a63e1892b950689dd70955f3aa1a3d9a
Jacob Crisan <jacob.crisan@gmail.com>
2020-02-29 00:05:16 -0800

    Initial Commit - Added something

---
+++ Temp.hs
@@ -0,0 +1,28 @@
+-- Convex Hull Algorithms take a set of points in some dimension and return a set such that the hull around the such that the rest of the points in the input set are contained within it 
+-- We will be working with several algorithms, including:
+--  - Optimal Algorithms
+--    * Chan's Algorithm
+--    * Divide and COnquer
+--  - Graham's Scan
+--  - Jarvis March
+--  etc
+-- 
+
+-- For easier referencing
+type Point2D = (Int, Int)
+
+-- 
+JarvisMarchHull :: [Point2D] -> [Point2D]
+JarvisMarchHull = undefined
+
+GrahmsScanHull :: [Point2D] -> [Point2D]
+GrahmsScanHull = undefined
+
+ChansAlgorithm :: [Point2D] -> [Point2D]
+ChansAlgorithm = undefined
+
+DivideAndConquerHull :: [Point2D] -> [Point2D]
+DivideAndConquerHull = undefined
+
+ChansAlgorithmHull :: [Point2D] -> [Point2D]
+ChansAlgorithmHull = undefined
