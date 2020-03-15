56e7e55ae2020454ae7981a7511f52b2f3049fad
Jacob Crisan <jacob.crisan@gmail.com>
2020-03-15 13:39:28 -0700

    Did some work to the algorithms. I didn't get them to work completely yet though

--- src/Algorithms.hs
+++ src/Algorithms.hs
@@ -2,6 +2,7 @@
 - File: Algorithms.hs
 - Date: 3/20/2020
 - Description: contains the Convex Hull Algorithms used in this project
+- Problem: these algorithms don't work right now
 -}
 
 module Algorithms where
@@ -13,26 +14,45 @@ import Data.List
 type Number = Double
 type Point2D = (Number, Number)
 
-{- Functions Common to one or more algorithms -}
--- Finds the point in the list with the lowest x and y coordinate
--- TODO: create QuickCheck test. Otherwise, it seems like it works
+-- To-Do: Clean out points that have the same polar angle. Pick the one farthest from the min point and filter (in here)
+-- 
 
--- Calculates the angle between two points, with respect to either the negative or positive x axis
--- INPUT:
---     axis: a boolean indicating whether to use the positive x axis
---     first: a point indicating if the starting point
---     second: a
--- https://stackoverflow.com/questions/2339487/calculate-angle-of-2-points/2339510
--- https://docs.python.org/2/library/cmath.html
--- https://hackage.haskell.org/package/cmath
-angle :: Bool -> Point2D -> Point2D -> (Number, Point2D)
-angle axis (x1, y1) (x2, y2) =
-    if (x1 == x2 && y1 == y2) then 
-        (20, (x2, y2))
-    else 
-        case axis of
-            True -> (atan2 (y2 - y1) (x2 - x1), (x2, y2))
-            False -> (atan2 (y1 - y2) (x2 - x1), (x2, y2))
+-- Sources/References:
+-- CLRS Texbook
+-- https://hackage.haskell.org/package/base-4.12.0.0/docs/Data-List.html
+-- https://mathworld.wolfram.com/PolarAngle.html
+-- https://stackoverflow.com/questions/2339487/calculate-angle-of-2-points/2339510 ?
+-- https://stackoverflow.com/questions/8791768/haskell-selecting-lists-from-a-list-when-a-condition-is-true ?
+-- https://math.stackexchange.com/questions/707673/find-angle-in-degrees-from-one-point-to-another-in-2d-space ?
+-- Functions Common to one or more algorithms 
+
+enoughPoints :: [Point2D] -> Bool
+enoughPoints coords = (length coords) > 2
+
+removeFromList :: Point2D -> [Point2D] -> [Point2D]
+removeFromList item [] = []
+removeFromList item (x:xs) = 
+    if x == item then
+        xs
+    else
+        [x] ++ (removeFromList item xs)
+
+-- https://www.mathsisfun.com/polar-cartesian-coordinates.html 
+polarAngle :: Bool -> Point2D -> Point2D -> Point2D -> Ordering
+polarAngle negativeXAxis (sx, sy) (x1, y1) (x2, y2) =  
+    if (angle1 > angle2) then
+        GT
+    else if (angle1 < angle2) then
+        LT
+    else
+        EQ
+    where
+        angle1 = 
+            if (negativeXAxis) then
+                atan ((y1 - sy) / (sx - x1)) -- not sure if this is right
+            else
+                atan ((y1 - sy) / (x1 - sx))
+        angle2 = atan ((y2 - sy) / (x2 - sx))
 
 {-
 - Jarvis March - A simple convex hull algorithm where we "gift warp"? our way around the set of points to find the convex hull
@@ -50,24 +70,98 @@ angle axis (x1, y1) (x2, y2) =
 - TODO: performance testing
 -}
 
-jarvisMarchGetChain :: Point2D -> Point2D -> Bool -> [Point2D] -> [Point2D]
-jarvisMarchGetChain start end isRightChain coords = 
-    if (hullPoint == end || hullPoint == start) then
+-- CLRS
+jarvisMarchConstructChain :: Bool -> [Point2D] -> [Point2D] -> Point2D -> [Point2D]
+jarvisMarchConstructChain isRightChain (x:xs) [] endPoint = (x:xs)
+jarvisMarchConstructChain isRightChain (x:xs) (y:ys) endPoint =
+    if (nextHullPoint == endPoint) then
+        (x:xs)
+    else 
+        jarvisMarchConstructChain isRightChain (nextHullPoint:x:xs) (drop 1 (y:ys)) endPoint
+    where
+        nextHullPoint = head (Data.List.sortBy (polarAngle isRightChain x) (removeFromList x (y:ys)))
+
+
+jarvisMarch :: [Point2D] -> [Point2D]
+jarvisMarch points =
+    if not (enoughPoints points) then
         []
     else
-        [hullPoint] ++ (jarvisMarchGetChain hullPoint end isRightChain coords)
-    where 
-        hullPoint = ((\(x, y) -> y) (Data.List.minimum (map (angle isRightChain start) coords)))
+        (jarvisMarchConstructChain False [minPoint] (removeFromList minPoint points) maxPoint)
+        ++
+        (jarvisMarchConstructChain True [maxPoint] (removeFromList maxPoint points) minPoint)
+        where
+            minPoint = Data.List.minimum points
+            maxPoint = Data.List.maximum points
 
 
-jarvisMarchHull :: [Point2D] -> [Point2D]
-jarvisMarchHull coords =
-    [minPoint] ++ (jarvisMarchGetChain minPoint maxPoint True coords) 
-    -- ++ [maxPoint] 
-    -- ++ (jarvisMarchGetChain maxPoint minPoint maxPoint False coords)
+{- Graham's Scan -}
+-- CLRS. "Introduction to Algorithms" Textbook
+makesLeftTurn :: Point2D -> Point2D -> Point2D -> Bool
+makesLeftTurn (sx, sy) (x1, y1) (x2, y2) =  
+    if (crossProduct < 0) then
+        True
+    else
+        False
     where
-        maxPoint = Data.List.maximum(coords)
-        minPoint = Data.List.minimum(coords)
+        x1m0 = (x1 - sx)
+        y1m0 = (y1 - sy)
+        x2m0 = (x2 - sx)
+        y2m0 = (y2 - sy)
+        crossProduct = x1m0 * y2m0 - x2m0 * y1m0 -- (p2 - p0) x (p1 - p0)
+
+-- Utter nonsense
+-- gramScanAddNextPoint
+gramScanCleanUpList :: {-Point2D -> [Point2D] -> -} Point2D -> [Point2D] -> [Point2D]
+-- gramScanCleanUpList newPoint [] = []
+gramScanCleanUpList newPoint (p1:p2:ps) = 
+    if not (makesLeftTurn newPoint p1 p2) then
+        gramScanCleanUpList newPoint (p2:ps)
+    else
+        newPoint:p1:p2:ps
+    --else
+    --    newPoint:p1:p2:p3:ps
+--gramScanCleanUpList newPoint (x:xs) = (x:xs)
+
+gramScanAddNextPoint :: [Point2D] -> [Point2D] -> [Point2D]
+gramScanAddNextPoint [] _ = []
+gramScanAddNextPoint (x:xs) [] = (x:xs)
+gramScanAddNextPoint (x:xs) (y:ys) = 
+    gramScanAddNextPoint cleanedList ys
+    where
+        cleanedList = gramScanCleanUpList y (x:xs)
+
+grahamScan :: [Point2D] -> [Point2D]
+grahamScan points = 
+    if not (enoughPoints points) then
+        []
+    else
+        (gramScanAddNextPoint (take 3 sortedPoints) ((drop 3 sortedPoints) ++ [minPoint]))
+        where
+            minPoint = Data.List.minimum points 
+            sortedPoints = Data.List.sortBy (polarAngle False minPoint) points
+            -- (removeFromList minPoint points)
+
+-- Finds the point in the list with the lowest x and y coordinate
+-- TODO: create QuickCheck test. Otherwise, it seems like it works
+
+-- Calculates the angle between two points, with respect to either the negative or positive x axis
+-- INPUT:
+--     axis: a boolean indicating whether to use the positive x axis
+--     first: a point indicating if the starting point
+--     second: a
+-- https://stackoverflow.com/questions/2339487/calculate-angle-of-2-points/2339510
+-- https://docs.python.org/2/library/cmath.html
+-- https://hackage.haskell.org/package/cmath
+{-angle :: Bool -> Point2D -> Point2D -> (Number, Point2D)
+angle axis (x1, y1) (x2, y2) =
+    if (x1 == x2 && y1 == y2) then 
+        (20, (x2, y2))
+    else 
+        case axis of
+            True -> (atan2 (y2 - y1) (x2 - x1), (x2, y2))
+            False -> (atan2 (y1 - y2) (x2 - x1), (x2, y2))-}
+
 
 --jarvisMarchHull [(0.0, 0.0), (1.0, 1.0), (2.0, 0.0), (2.0, 3.0), (0.0, 3.0), (1.0, 2.0)]
 -- Calculate polar angle form starting point for every point in coords (with a map), then get the max of that 
